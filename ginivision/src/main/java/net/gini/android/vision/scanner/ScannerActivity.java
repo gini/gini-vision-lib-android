cd9876c8d837c8f59cfabd24550b626917bb18a0
Alpar Szotyori <a.szotyori@gini.net>
2016-06-08 17:50:32 +0200

    Onboarding menu item and icon

--- ginivision/src/main/java/net/gini/android/vision/scanner/ScannerActivity.java
+++ ginivision/src/main/java/net/gini/android/vision/scanner/ScannerActivity.java
@@ -2,6 +2,7 @@ package net.gini.android.vision.scanner;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
 
 import net.gini.android.vision.R;
 
@@ -11,5 +12,11 @@ public class ScannerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.gv_activity_scanner);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.gv_scanner, menu);
+        return true;
     }
 }
