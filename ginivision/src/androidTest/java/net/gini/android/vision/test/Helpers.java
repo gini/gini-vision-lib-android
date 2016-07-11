7d5729ea35157501ebf3e45e27ed25c31dde1f02
Alpar Szotyori <a.szotyori@gini.net>
2016-07-11 12:49:13 +0200

    Wrote test for OnboardingPage parceling

---
+++ ginivision/src/androidTest/java/net/gini/android/vision/test/Helpers.java
@@ -0,0 +1,14 @@
+package net.gini.android.vision.test;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Helpers {
+
+    public static <T extends Parcelable, C extends Parcelable.Creator<T>> T doParcelingRoundTrip(T payload, C creator) {
+        Parcel parcel = Parcel.obtain();
+        payload.writeToParcel(parcel, 0);
+        parcel.setDataPosition(0);
+        return creator.createFromParcel(parcel);
+    }
+}
