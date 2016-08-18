57421363f78113a8b651b68485b041804aa92ca2
Alpar Szotyori <a.szotyori@gini.net>
2016-08-18 14:59:42 +0200

    Rewrote the Component API example app to show usage of the fragments
    Extractions are retrieved and shown and feedback can also be sent.
    
    Using a standard and a compat multi-fragment activity for showcasing.

---
+++ componentapiexample/src/main/java/net/gini/android/vision/component/ExtractionsActivity.java
@@ -0,0 +1,248 @@
+package net.gini.android.vision.component;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import net.gini.android.DocumentTaskManager;
+import net.gini.android.Gini;
+import net.gini.android.SdkBuilder;
+import net.gini.android.models.Document;
+import net.gini.android.models.Extraction;
+import net.gini.android.models.SpecificExtraction;
+import net.gini.android.visionadvtest.R;
+
+import org.json.JSONException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import bolts.Continuation;
+import bolts.Task;
+
+/**
+ * <p>
+ *     Displays the Pay5 extractions: paymentRecipient, iban, bic, amount and paymentReference.
+ * </p>
+ * <p>
+ *     A menu item is added to send feedback. The amount is changed to 10.00:EUR or an amount of 10.00:EUR is added, if missing.
+ * </p>
+ */
+public class ExtractionsActivity extends AppCompatActivity {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ExtractionsActivity.class);
+
+    public static final String EXTRA_IN_DOCUMENT = "EXTRA_IN_DOCUMENT";
+    public static final String EXTRA_IN_EXTRACTIONS = "EXTRA_IN_EXTRACTIONS";
+
+    private Map<String, SpecificExtraction> mExtractions = new HashMap<>();
+    private Gini mGiniApi;
+
+    private RecyclerView mRecyclerView;
+    private LinearLayout mLayoutProgress;
+
+    private ExtractionsAdapter mExtractionsAdapter;
+    private Document mDocument;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_extractions);
+        readExtras();
+        bindViews();
+        setUpRecyclerView();
+        createGiniApi();
+    }
+
+    private void createGiniApi() {
+        SdkBuilder builder = new SdkBuilder(this,
+                this.getString(R.string.gini_api_client_id),
+                this.getString(R.string.gini_api_client_secret),
+                "example.com");
+        mGiniApi = builder.build();
+    }
+
+    private void bindViews() {
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerview_extractions);
+        mLayoutProgress = (LinearLayout) findViewById(R.id.layout_progress);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu_extractions, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.feedback) {
+            sendFeedback();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void readExtras() {
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            Bundle extractionsBundle = extras.getParcelable(EXTRA_IN_EXTRACTIONS);
+            if (extractionsBundle != null) {
+                for (String key : extractionsBundle.keySet()) {
+                    mExtractions.put(key, (SpecificExtraction) extractionsBundle.getParcelable(key));
+                }
+            }
+            mDocument = extras.getParcelable(EXTRA_IN_DOCUMENT);
+        }
+    }
+
+    private void setUpRecyclerView() {
+        //noinspection ConstantConditions
+        mRecyclerView.setHasFixedSize(true);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(layoutManager);
+
+        // Show only the "Pay5" extractions: recipient, iban, bic, reference, amount
+        mExtractionsAdapter = new ExtractionsAdapter(getSortedPay5Extractions());
+        mRecyclerView.setAdapter(mExtractionsAdapter);
+    }
+
+    private List<SpecificExtraction> getSortedPay5Extractions() {
+        ArrayList<SpecificExtraction> sortedExtractions = new ArrayList<>();
+        ArrayList<String> keys = new ArrayList<>(mExtractions.keySet());
+        // Ascending order
+        Collections.sort(keys);
+        for (String key : keys) {
+            if (key.equals("amountToPay") ||
+                    key.equals("bic") ||
+                    key.equals("iban") ||
+                    key.equals("paymentReference") ||
+                    key.equals("paymentRecipient")) {
+                sortedExtractions.add(mExtractions.get(key));
+            }
+        }
+        return sortedExtractions;
+    }
+
+    private void sendFeedback() {
+        DocumentTaskManager documentTaskManager = mGiniApi.getDocumentTaskManager();
+
+        SpecificExtraction amount = mExtractions.get("amountToPay");
+        if (amount != null) {
+            // Let's assume the amount was wrong and change it
+            amount.setValue("10.00:EUR");
+            Toast.makeText(this, "Amount changed to 10.00:EUR", Toast.LENGTH_SHORT).show();
+        } else {
+            // Amount was missing, let's add it
+            SpecificExtraction extraction = new SpecificExtraction("amountToPay", "10.00:EUR", "amount", null, Collections.<Extraction>emptyList());
+            mExtractions.put("amountToPay", extraction);
+            mExtractionsAdapter.setExtractions(getSortedPay5Extractions());
+            Toast.makeText(this, "Added amount of 10.00:EUR", Toast.LENGTH_SHORT).show();
+        }
+        mExtractionsAdapter.notifyDataSetChanged();
+
+        // We require the Gini API SDK's net.gini.android.models.Document for sending the feedback
+        if (mDocument != null) {
+            try {
+                showProgressIndicator();
+                documentTaskManager.sendFeedbackForExtractions(mDocument, mExtractions)
+                        .continueWith(new Continuation<Document, Object>() {
+                            @Override
+                            public Object then(final Task<Document> task) throws Exception {
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        if (task.isFaulted()) {
+                                            LOG.error("Feedback error", task.getError());
+                                            String message = "unknown";
+                                            if (task.getError() != null) {
+                                                message = task.getError().getMessage();
+                                            }
+                                            Toast.makeText(ExtractionsActivity.this, "Feedback error:\n" + message, Toast.LENGTH_LONG).show();
+                                        } else {
+                                            Toast.makeText(ExtractionsActivity.this, "Feedback successful", Toast.LENGTH_LONG).show();
+                                        }
+                                        hideProgressIndicator();
+                                    }
+                                });
+                                return null;
+                            }
+                        });
+            } catch (JSONException e) {
+                LOG.error("Feedback not sent", e);
+                Toast.makeText(this, "Feedback not set:\n" + e.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        } else {
+            Toast.makeText(this, "Feedback not set: no Gini Api Document available", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void showProgressIndicator() {
+        mRecyclerView.animate().alpha(0.5f);
+        mLayoutProgress.setVisibility(View.VISIBLE);
+    }
+
+    private void hideProgressIndicator() {
+        mRecyclerView.animate().alpha(1.0f);
+        mLayoutProgress.setVisibility(View.GONE);
+    }
+
+    private class ExtractionsAdapter extends RecyclerView.Adapter<ExtractionsAdapter.ExtractionsViewHolder> {
+
+        class ExtractionsViewHolder extends RecyclerView.ViewHolder {
+
+            public TextView mTextName;
+            public TextView mTextValue;
+
+            public ExtractionsViewHolder(View itemView) {
+                super(itemView);
+
+                mTextName = (TextView) itemView.findViewById(R.id.text_name);
+                mTextValue = (TextView) itemView.findViewById(R.id.text_value);
+            }
+        }
+
+        private List<SpecificExtraction> mExtractions;
+
+        private ExtractionsAdapter(List<SpecificExtraction> extractions) {
+            mExtractions = extractions;
+        }
+
+        public void setExtractions(List<SpecificExtraction> extractions) {
+            mExtractions = extractions;
+        }
+
+        @Override
+        public ExtractionsViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+            return new ExtractionsViewHolder(layoutInflater.inflate(R.layout.item_extraction, parent, false));
+        }
+
+        @Override
+        public void onBindViewHolder(ExtractionsViewHolder holder, int position) {
+            holder.mTextName.setText(mExtractions.get(position).getName());
+            holder.mTextValue.setText(mExtractions.get(position).getValue());
+        }
+
+        @Override
+        public int getItemCount() {
+            return mExtractions.size();
+        }
+
+    }
+}
--- componentapiexample/src/main/java/net/gini/android/vision/component/MainActivity.java
+++ componentapiexample/src/main/java/net/gini/android/vision/component/MainActivity.java
@@ -12,16 +12,8 @@ import net.gini.android.visionadvtest.R;
 
 public class MainActivity extends Activity {
 
-    private Button mButtonStartScanner;
-    private Button mButtonStartScannerCompat;
-    private Button mButtonStartOnboarding;
-    private Button mButtonStartOnboardingWithoutLastPage;
-    private Button mButtonStartOnboardingCompat;
-    private Button mButtonStartOnboardingWithoutLastPageCompat;
-    private Button mButtonStartReviewDocument;
-    private Button mButtonStartReviewDocumentCompat;
-    private Button mButtonStartAnalyzeDocument;
-    private Button mButtonStartAnalyzeDocumentCompat;
+    private Button mButtonStartGiniVisionStandard;
+    private Button mButtonStartGiniVisionCompat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,130 +31,32 @@ public class MainActivity extends Activity {
     }
 
     private void addInputHandlers() {
-        mButtonStartScanner.setOnClickListener(new View.OnClickListener() {
+        mButtonStartGiniVisionStandard.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startScanner();
+                startGiniVisionStandard();
             }
         });
-        mButtonStartScannerCompat.setOnClickListener(new View.OnClickListener() {
+        mButtonStartGiniVisionCompat.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startScannerCompat();
-            }
-        });
-        mButtonStartOnboarding.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startOnboarding();
-            }
-        });
-        mButtonStartOnboardingWithoutLastPage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startOnboardingWithoutLastPage();
-            }
-        });
-        mButtonStartOnboardingCompat.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startOnboardingCompat();
-            }
-        });
-        mButtonStartOnboardingWithoutLastPageCompat.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startOnboardingWithoutLastPageCompat();
-            }
-        });
-        mButtonStartReviewDocument.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startReviewDocument();
-            }
-        });
-        mButtonStartReviewDocumentCompat.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startReviewDocumentCompat();
-            }
-        });
-        mButtonStartAnalyzeDocument.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startAnalyzeDocument();
-            }
-        });
-        mButtonStartAnalyzeDocumentCompat.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startAnalyzeDocumentCompat();
+                startGiniVisionCompat();
             }
         });
     }
 
-    private void startScanner() {
-        Intent intent = new Intent(this, CustomCameraActivity.class);
+    private void startGiniVisionStandard() {
+        Intent intent = new Intent(this, GiniVisionActivity.class);
         startActivity(intent);
     }
 
-    private void startScannerCompat() {
-        Intent intent = new Intent(this, CustomCameraAppCompatActivity.class);
-        startActivity(intent);
-    }
-
-    private void startOnboarding() {
-        Intent intent = new Intent(this, CustomOnboardingActivity.class);
-        startActivity(intent);
-    }
-
-    private void startOnboardingWithoutLastPage() {
-        Intent intent = new Intent(this, CustomOnboardingActivity.class);
-        intent.putExtra(CustomOnboardingActivity.EXTRA_WITHOUT_EMPTY_LAST_PAGE, true);
-        startActivity(intent);
-    }
-
-    private void startOnboardingCompat() {
-        Intent intent = new Intent(this, CustomOnboardingAppCompatActivity.class);
-        startActivity(intent);
-    }
-
-    private void startOnboardingWithoutLastPageCompat() {
-        Intent intent = new Intent(this, CustomOnboardingAppCompatActivity.class);
-        intent.putExtra(CustomOnboardingAppCompatActivity.EXTRA_WITHOUT_EMPTY_LAST_PAGE, true);
-        startActivity(intent);
-    }
-
-    private void startReviewDocument() {
-        Intent intent = new Intent(this, CustomReviewActivity.class);
-        startActivity(intent);
-    }
-
-    private void startReviewDocumentCompat() {
-        Intent intent = new Intent(this, CustomReviewAppCompatActivity.class);
-        startActivity(intent);
-    }
-
-    private void startAnalyzeDocument() {
-        Intent intent = new Intent(this, CustomAnalysisActivity.class);
-        startActivity(intent);
-    }
-
-    private void startAnalyzeDocumentCompat() {
-        Intent intent = new Intent(this, CustomAnalysisAppCompatActivity.class);
+    private void startGiniVisionCompat() {
+        Intent intent = new Intent(this, GiniVisionAppCompatActivity.class);
         startActivity(intent);
     }
 
     private void bindViews() {
-        mButtonStartScanner = (Button) findViewById(R.id.button_start_camera);
-        mButtonStartScannerCompat = (Button) findViewById(R.id.button_start_camera_compat);
-        mButtonStartOnboarding = (Button) findViewById(R.id.button_start_onboarding);
-        mButtonStartOnboardingWithoutLastPage = (Button) findViewById(R.id.button_start_onboarding_without_last_page);
-        mButtonStartOnboardingCompat = (Button) findViewById(R.id.button_start_onboarding_compat);
-        mButtonStartOnboardingWithoutLastPageCompat = (Button) findViewById(R.id.button_start_onboarding_without_last_page_compat);
-        mButtonStartReviewDocument = (Button) findViewById(R.id.button_start_review_document);
-        mButtonStartReviewDocumentCompat = (Button) findViewById(R.id.button_start_review_document_compat);
-        mButtonStartAnalyzeDocument = (Button) findViewById(R.id.button_start_analysis);
-        mButtonStartAnalyzeDocumentCompat = (Button) findViewById(R.id.button_start_analysis_compat);
+        mButtonStartGiniVisionStandard = (Button) findViewById(R.id.button_start_gini_vision_standard);
+        mButtonStartGiniVisionCompat = (Button) findViewById(R.id.button_start_gini_vision_compat);
     }
 }
