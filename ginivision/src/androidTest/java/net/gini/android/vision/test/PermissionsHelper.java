86530f9a00944067be3349423c5ed45b4ddf60df
Alpar Szotyori <a.szotyori@gini.net>
2017-01-20 18:50:59 +0100

    Permission can be granted for arbitrary packages

--- ginivision/src/androidTest/java/net/gini/android/vision/test/PermissionsHelper.java
+++ ginivision/src/androidTest/java/net/gini/android/vision/test/PermissionsHelper.java
@@ -7,26 +7,37 @@ import android.os.Build;
 
 public class PermissionsHelper {
 
-    private static final long GRANT_CAMERA_PERMISSION_PAUSE_DURATION = 500;
+    private static final long GRANT_PERMISSION_PAUSE_DURATION = 500;
 
-    public static void grantExternalStoragePermission() {
+    public static void grantExternalStoragePermission() throws InterruptedException {
+        grantExternalStoragePermission(getTargetContext().getPackageName());
+    }
+
+    public static void grantExternalStoragePermission(final String packageName)
+            throws InterruptedException {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             getInstrumentation().getUiAutomation().executeShellCommand(
-                    "pm grant " + getTargetContext().getPackageName()
+                    "pm grant " + packageName
                             + " android.permission.READ_EXTERNAL_STORAGE");
             getInstrumentation().getUiAutomation().executeShellCommand(
-                    "pm grant " + getTargetContext().getPackageName()
+                    "pm grant " + packageName
                             + " android.permission.WRITE_EXTERNAL_STORAGE");
         }
+        // A delay is needed for the permission
+        Thread.sleep(GRANT_PERMISSION_PAUSE_DURATION);
     }
 
     public static void grantCameraPermission() throws InterruptedException {
+        grantCameraPermission(getTargetContext().getPackageName());
+    }
+
+    public static void grantCameraPermission(final String packageName) throws InterruptedException {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             getInstrumentation().getUiAutomation().executeShellCommand(
-                    "pm grant " + getTargetContext().getPackageName()
+                    "pm grant " + packageName
                             + " android.permission.CAMERA");
         }
-        // A delay is needed for the camera permission
-        Thread.sleep(GRANT_CAMERA_PERMISSION_PAUSE_DURATION);
+        // A delay is needed for the permission
+        Thread.sleep(GRANT_PERMISSION_PAUSE_DURATION);
     }
 }
