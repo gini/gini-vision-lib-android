7bc7ecd69456efae8989c3be4758fabc57019622
Alpar Szotyori <a.szotyori@gini.net>
2017-01-20 14:56:43 +0100

    Added listing resolutions for failed resolution requirement check

--- ginivision/src/androidTest/java/net/gini/android/vision/requirements/GiniVisionRequirementsTest.java
+++ ginivision/src/androidTest/java/net/gini/android/vision/requirements/GiniVisionRequirementsTest.java
@@ -4,11 +4,15 @@ import static com.google.common.truth.Truth.assertWithMessage;
 
 import static net.gini.android.vision.test.PermissionsHelper.grantCameraPermission;
 
+import android.hardware.Camera;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.List;
+import java.util.Locale;
 
 @RunWith(AndroidJUnit4.class)
 public class GiniVisionRequirementsTest {
@@ -39,8 +43,43 @@ public class GiniVisionRequirementsTest {
                     stringBuilder.append(requirementReport.getDetails());
                     stringBuilder.append("\n");
                 }
+                if (requirementReport.getRequirementId() == RequirementId.CAMERA_RESOLUTION) {
+                    appendResolutions(stringBuilder);
+                }
             }
         }
         return stringBuilder.toString();
+    }
+
+    private void appendResolutions(final StringBuilder stringBuilder) {
+        Camera camera = Camera.open();
+        stringBuilder.append("Picture resolutions:\n");
+        appendPictureResolutions(stringBuilder, camera);
+        stringBuilder.append("Preview resolutions:\n");
+        appendPreviewResolutions(stringBuilder, camera);
+        if (camera != null) {
+            camera.release();
+        }
+    }
+
+    private void appendPictureResolutions(final StringBuilder stringBuilder, final Camera camera) {
+        final List<Camera.Size> sizes = camera.getParameters().getSupportedPictureSizes();
+        appendSizes(stringBuilder, sizes);
+    }
+
+    private void appendSizes(final StringBuilder stringBuilder, final List<Camera.Size> sizes) {
+        stringBuilder.append("Resolution, Megapixel, Aspect Ratio\n");
+        for (final Camera.Size size : sizes) {
+            stringBuilder.append(
+                    String.format(Locale.US, "%dx%d, %.1fMP, %.6f", size.width, size.height,
+                            (float) size.width * size.height / 1_000_000f,
+                            (float) size.width / size.height))
+                    .append("\n");
+        }
+    }
+
+    private void appendPreviewResolutions(final StringBuilder stringBuilder, final Camera camera) {
+        final List<Camera.Size> sizes = camera.getParameters().getSupportedPreviewSizes();
+        appendSizes(stringBuilder, sizes);
     }
 }
