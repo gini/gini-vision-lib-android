d289202be34a5787aedaa11c56c71e8304671e12
Alpár Szotyori <a.szotyori@gini.net>
2017-07-10 18:33:30 +0200

    Coverage downloadable w/o root access

--- scripts/targeted-android-tests.sh
+++ scripts/targeted-android-tests.sh
@@ -11,7 +11,7 @@ results_dir=${module}/build/outputs/androidTest-results/targeted
 results_file=${target}-${module}-${variant}.txt
 test_results=${results_dir}/${results_file}
 
-on_device_coverage_file=/data/data/${package}.test/files/coverage.ec
+on_device_coverage_file=/data/data/${package}.test/coverage.ec
 coverage_dir=${module}/build/outputs/code-coverage/targeted
 coverage_file=${target}-${module}-${variant}-coverage.ec
 coverage_results=${coverage_dir}/${coverage_file}
@@ -26,8 +26,14 @@ adb -s ${target} shell pm install -r "/data/local/tmp/${package}.test"
 mkdir -p ${results_dir}
 adb -s ${target} shell am instrument -w -r -e package ${package} -e debug false -e coverage true -e coverageFile ${on_device_coverage_file} ${package}.test/android.support.test.runner.AndroidJUnitRunner > ${test_results}
 
+on_device_temp_coverage_file=/data/local/tmp/${package}.test.coverage.ec
+
+adb -s ${target} shell "run-as ${package}.test cat ${on_device_coverage_file} | cat > ${on_device_temp_coverage_file}"
+
 mkdir -p ${coverage_dir}
-adb -s ${target} pull ${on_device_coverage_file} ${coverage_results}
+adb -s ${target} pull ${on_device_temp_coverage_file} ${coverage_results}
+
+adb -s ${target} shell "rm ${on_device_temp_coverage_file}"
 
 adb -s ${target} shell pm uninstall ${package}.test || true
 
