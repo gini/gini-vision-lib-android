860322c87fe57fb47279559f53910f6b83ebe782
Alpár Szotyori <a.szotyori@gini.net>
2017-08-04 16:38:46 +0200

    Photo display rotation set according to the orientation
    Issue #49

--- ginivision/src/main/java/net/gini/android/vision/internal/camera/api/CameraController.java
+++ ginivision/src/main/java/net/gini/android/vision/internal/camera/api/CameraController.java
@@ -352,7 +352,7 @@ public class CameraController implements CameraInterface {
                     @Override
                     public void onPictureTaken(final byte[] bytes, Camera camera) {
                         mTakingPictureFuture.set(null);
-                        final Photo photo = Photo.fromJpeg(bytes, getBackFacingCameraOrientation());
+                        final Photo photo = Photo.fromJpeg(bytes, getDisplayOrientationForCamera(mActivity));
                         LOG.info("Picture taken");
                         pictureTaken.complete(photo);
                     }
@@ -445,9 +445,16 @@ public class CameraController implements CameraInterface {
 
     private void setCameraDisplayOrientation(Activity activity, android.hardware.Camera camera) {
         LOG.debug("Setting camera display orientation");
+        final int displayOrientation = getDisplayOrientationForCamera(activity);
+        camera.setDisplayOrientation(displayOrientation);
+        LOG.debug("Camera display orientation set to {}", displayOrientation);
+    }
+
+    private int getDisplayOrientationForCamera(Activity activity) {
         Camera.CameraInfo info = getBackFacingCameraInfo();
         if (info == null) {
-            return;
+            LOG.error("Could not get back facing camera info");
+            return 0;
         }
         int rotation = activity.getWindowManager().getDefaultDisplay()
                 .getRotation();
@@ -466,7 +473,7 @@ public class CameraController implements CameraInterface {
                 degrees = 270;
                 break;
         }
-        LOG.debug("Default display rotation {}", degrees);
+        LOG.debug("Default display rotation is {}", degrees);
 
         int result;
         if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
@@ -475,8 +482,8 @@ public class CameraController implements CameraInterface {
         } else {  // back-facing
             result = (info.orientation - degrees + 360) % 360;
         }
-        camera.setDisplayOrientation(result);
-        LOG.debug("Camera display orientation set to {}", result);
+
+        return result;
     }
 
     @Nullable
