7be82ee67d05ffb98d7aa1fa228c787b687d29e8
Alpár Szotyori <a.szotyori@gini.net>
2017-08-17 19:51:53 +0200

    Added scripts for creating avds in the project folder
    Issue #52

---
+++ scripts/get-avd-name.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+#
+# Creates an avd name as follows:
+#   {working dir name}_{branch name} 
+#   ex: gini-vision-lib-android_feature-tablet-support
+#
+# Must be executed from the project root.
+# 
+set -e
+#set -x
+
+branch_name=$(git describe --all --exact-match HEAD | sed -E "s:(heads\/|remotes\/|tags\/|origin\/)::g")
+dir_name=$(basename "$(pwd)")
+avd_name="$dir_name"_"$branch_name"
+
+echo "$avd_name"
