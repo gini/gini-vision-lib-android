033c8b2553736c212367f3049c1bf2351a6492bc
Alpár Szotyori <a.szotyori@gini.net>
2017-09-29 18:29:00 +0200

    Methods for launching with an imported file via "open with"
    Issue #88

--- ginivision/src/main/java/net/gini/android/vision/internal/util/DeviceHelper.java
+++ ginivision/src/main/java/net/gini/android/vision/internal/util/DeviceHelper.java
@@ -1,17 +1,18 @@
 package net.gini.android.vision.internal.util;
 
-import android.app.Activity;
+import android.content.Context;
+import android.support.annotation.NonNull;
 
 /**
  * @exclude
  */
 public final class DeviceHelper {
 
-    public static String getDeviceOrientation(Activity activity) {
-        return ContextHelper.isPortraitOrientation(activity) ? "portrait" : "landscape";
+    public static String getDeviceOrientation(@NonNull final Context context) {
+        return ContextHelper.isPortraitOrientation(context) ? "portrait" : "landscape";
     }
 
-    public static String getDeviceType(Activity activity) {
-        return ContextHelper.isTablet(activity) ? "tablet" : "phone";
+    public static String getDeviceType(@NonNull final Context context) {
+        return ContextHelper.isTablet(context) ? "tablet" : "phone";
     }
 }
