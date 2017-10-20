1470f4d27074bd70b2cfd4f843ab1cf7f9e69ecf
Alpár Szotyori <a.szotyori@gini.net>
2017-10-20 11:15:31 +0200

    Updated No Results Screen documentation

--- ginivision/src/main/java/net/gini/android/vision/noresults/NoResultsActivity.java
+++ ginivision/src/main/java/net/gini/android/vision/noresults/NoResultsActivity.java
@@ -20,6 +20,87 @@ import net.gini.android.vision.review.ReviewActivity;
  * When you use the Screen API, the {@code NoResultsFragmentCompat} displays hints that show how to
  * best take a picture of a document.
  * </p>
+ *
+ * <h3>Customizing the No Results Screen</h3>
+ *
+ * <p>
+ *   Customizing the look of the No Results Screen is done via overriding of app resources.
+ * </p>
+ * <p>
+ *     The following items are customizable:
+ *     <ul>
+ *         <li>
+ *             <b>Header text color:</b> via the color resource named {@code
+ *             gv_noresults_header}
+ *         </li>
+ *         <li>
+ *             <b>Header text font:</b> via overriding the style named {@code GiniVisionTheme
+ *             .NoResults.Header.TextStyle} and setting an item named {@code gvCustomFont} with the path to the font file in your {@code assets} folder
+ *         </li>
+ *         <li>
+ *             <b>Header text style:</b> via overriding the style named {@code GiniVisionTheme.NoResults.Header.TextStyle} and setting an item named {@code android:textStyle} to {@code normal}, {@code bold} or {@code italic}
+ *         </li>
+ *         <li>
+ *             <b>Header text size:</b> via overriding the style named {@code GiniVisionTheme.NoResults.Header.TextStyle} and setting an item named {@code android:textSize} to the desired {@code sp} size
+ *         </li>
+ *         <li>
+ *             <b>Headline text color:</b> via the color resource named {@code gv_noresults_headline}
+ *         </li>
+ *         <li>
+ *             <b>Headline text font:</b> via overriding the style named {@code GiniVisionTheme.NoResults.Headline.TextStyle} and setting an item named {@code gvCustomFont} with the path to the font file in your {@code assets} folder
+ *         </li>
+ *         <li>
+ *             <b>Headline text style:</b> via overriding the style named {@code GiniVisionTheme.NoResults.Headline.TextStyle} and setting an item named {@code android:textStyle} to {@code normal}, {@code bold} or {@code italic}
+ *         </li>
+ *         <li>
+ *             <b>Headline text size:</b> via overriding the style named {@code GiniVisionTheme.NoResults.Headline.TextStyle} and setting an item named {@code android:textSize} to the desired {@code sp} size
+ *         </li>
+ *         <li>
+ *             <b>Tip text color:</b> via the color resource named {@code gv_noresults_tip}
+ *         </li>
+ *         <li>
+ *             <b>Tip text font:</b> via overriding the style named {@code GiniVisionTheme.NoResults.Tip.TextStyle} and setting an item named {@code gvCustomFont} with the path to the font file in your {@code assets} folder
+ *         </li>
+ *         <li>
+ *             <b>Tip text style:</b> via overriding the style named {@code GiniVisionTheme.NoResults.Tip.TextStyle} and setting an item named {@code android:textStyle} to {@code normal}, {@code bold} or {@code italic}
+ *         </li>
+ *         <li>
+ *             <b>Tip text size:</b> via overriding the style named {@code GiniVisionTheme.NoResults.Tip.TextStyle} and setting an item named {@code android:textSize} to the desired {@code sp} size
+ *         </li>
+ *         <li>
+ *             <b>Tip image - Good lighting:</b> via images for mdpi, hdpi, xhdpi, xxhdpi, xxxhdpi
+ *             named
+ *             {@code gv_noresults_lighting.png}
+ *         </li>
+ *         <li>
+ *             <b>Tip image - Document should be flat:</b> via images for mdpi, hdpi, xhdpi, xxhdpi,
+ *             xxxhdpi
+ *             named {@code gv_noresults_flat.png}
+ *         </li>
+ *         <li>
+ *             <b>Tip image - Device should be parallel to document:</b> via images for mdpi, hdpi,
+ *             xhdpi,xxhdpi, xxxhdpi named {@code gv_noresults_parallel.png}
+ *         </li>
+ *         <li>
+ *             <b>Tip image - Document should be aligned with corner guides:</b> via
+ *             images for mdpi, hdpi, xhdpi,xxhdpi, xxxhdpi named {@code gv_noresults_align.png}
+ *         </li>
+ *         <li>
+ *             <b>Button color:</b> via the color resource named {@code gv_noresults_button}
+ *         </li>
+ *         <li>
+ *             <b>Button text color:</b> via the color resource named {@code gv_noresults_button_text}
+ *         </li>
+ *         <li>
+ *             <b>Background color:</b> via the color resource named {@code gv_noresults_background}.
+ *         </li>
+ *     </ul>
+ * </p>
+ *
+ * <p>
+ *     <b>Important:</b> All overriden styles must have their respective {@code Root.} prefixed style as their parent. Ex.: the parent of {@code GiniVisionTheme.Onboarding.Message.TextStyle} must be {@code Root.GiniVisionTheme.Onboarding.Message.TextStyle}.
+ * </p>
+ *
  * <h3>Customizing the Action Bar</h3>
  *
  * <p>
@@ -33,12 +114,6 @@ import net.gini.android.vision.review.ReviewActivity;
  * <li>
  * <b>Background color:</b> via the color resource named {@code gv_action_bar} (highly recommended
  * for Android 5+: customize the status bar color via {@code gv_status_bar})
- * </li>
- * <li>
- * <b>Title:</b> via the string resource name {@code gv_title_noresults}
- * </li>
- * <li>
- * <b>Title color:</b> via the color resource named {@code gv_action_bar_title}
  * </li>
  * </ul>
  * </p>
