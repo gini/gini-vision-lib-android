bbf006174b7590f26ac1873103f68395cf42c175
Alpár Szotyori <a.szotyori@gini.net>
2017-10-24 15:07:23 +0200

    File Import guide section 1 can show an illustration, if available (height > 0)

--- ginivision/src/main/java/net/gini/android/vision/help/FileImportActivity.java
+++ ginivision/src/main/java/net/gini/android/vision/help/FileImportActivity.java
@@ -1,7 +1,11 @@
 package net.gini.android.vision.help;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 import net.gini.android.vision.R;
 
@@ -11,5 +15,14 @@ public class FileImportActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.gv_activity_file_import);
+
+        // Show illustration for the first section only if available (height > 0)
+        final Drawable section1Illustration = ContextCompat.getDrawable(this,
+                R.drawable.gv_file_import_section_1_illustration);
+        if (section1Illustration.getMinimumHeight() > 0) {
+            final ImageView section1ImageView = findViewById(R.id.gv_section_1_illustration);
+            section1ImageView.setVisibility(View.VISIBLE);
+            section1ImageView.setImageDrawable(section1Illustration);
+        }
     }
 }
