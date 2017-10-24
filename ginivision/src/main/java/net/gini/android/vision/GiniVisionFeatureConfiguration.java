c039e5c2b48442ebc68280b8f310bc1802e8ff6c
Alpár Szotyori <a.szotyori@gini.net>
2017-10-24 17:54:06 +0200

    Added javadoc for the feature configuration

--- ginivision/src/main/java/net/gini/android/vision/GiniVisionFeatureConfiguration.java
+++ ginivision/src/main/java/net/gini/android/vision/GiniVisionFeatureConfiguration.java
@@ -6,22 +6,31 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
 /**
- *
+ * <p>
+ *     Using this class the features of the Gini Vision Library can be configured.
+ * </p>
+ * <p>
+ *     Use the builder by invoking {@link GiniVisionFeatureConfiguration#buildNewConfiguration()} to
+ *     configure and create a new instance.
+ * </p>
  */
-
 public class GiniVisionFeatureConfiguration implements Parcelable {
 
-    public static final Creator<GiniVisionFeatureConfiguration> CREATOR = new Creator<GiniVisionFeatureConfiguration>() {
-        @Override
-        public GiniVisionFeatureConfiguration createFromParcel(Parcel in) {
-            return new GiniVisionFeatureConfiguration(in);
-        }
+    /**
+     * @exclude
+     */
+    public static final Creator<GiniVisionFeatureConfiguration> CREATOR =
+            new Creator<GiniVisionFeatureConfiguration>() {
+                @Override
+                public GiniVisionFeatureConfiguration createFromParcel(Parcel in) {
+                    return new GiniVisionFeatureConfiguration(in);
+                }
 
-        @Override
-        public GiniVisionFeatureConfiguration[] newArray(int size) {
-            return new GiniVisionFeatureConfiguration[size];
-        }
-    };
+                @Override
+                public GiniVisionFeatureConfiguration[] newArray(int size) {
+                    return new GiniVisionFeatureConfiguration[size];
+                }
+            };
 
     private final DocumentImportEnabledFileTypes mDocumentImportEnabledFileTypes;
     private final boolean mFileImportEnabled;
@@ -36,31 +45,66 @@ public class GiniVisionFeatureConfiguration implements Parcelable {
         mFileImportEnabled = builder.isFileImportEnabled();
     }
 
-    @NonNull
-    public DocumentImportEnabledFileTypes getDocumentImportEnabledFileTypes() {
-        return mDocumentImportEnabledFileTypes;
-    }
-
-    public boolean isFileImportEnabled() {
-        return mFileImportEnabled;
-    }
-
+    /**
+     * @exclude
+     */
     @Override
     public int describeContents() {
         return 0;
     }
 
+    /**
+     * @exclude
+     */
     @Override
     public void writeToParcel(final Parcel parcel, final int i) {
         parcel.writeSerializable(mDocumentImportEnabledFileTypes);
         parcel.writeByte((byte) (mFileImportEnabled ? 1 : 0));
     }
 
+    /**
+     * <p>
+     *     Builder for creating a new feature configuration.
+     * </p>
+     * @return a builder for creating a new feature configuration
+     */
     @NonNull
     public static Builder buildNewConfiguration() {
         return new Builder();
     }
 
+    /**
+     * <p>
+     *     Retrieve the file types enabled for document import.
+     * </p>
+     * <p>
+     *     Disabled by default.
+     * </p>
+     * @return enabled file types
+     */
+    @NonNull
+    public DocumentImportEnabledFileTypes getDocumentImportEnabledFileTypes() {
+        return mDocumentImportEnabledFileTypes;
+    }
+
+    /**
+     * <p>
+     *     Find out whether file import has been enabled.
+     * </p>
+     * <p>
+     *     Disabled by default.
+     * </p>
+     * @return {@code true} if file import was enabled
+     */
+    public boolean isFileImportEnabled() {
+        return mFileImportEnabled;
+    }
+
+    /**
+     * <p>
+     *     Feature configuration builder.
+     * </p>
+     */
     public static class Builder {
 
         private DocumentImportEnabledFileTypes mDocumentImportEnabledFileTypes =
@@ -70,6 +114,12 @@ public class GiniVisionFeatureConfiguration implements Parcelable {
         private Builder() {
         }
 
+        /**
+         * <p>
+         *     Create a new {@link GiniVisionFeatureConfiguration} instance.
+         * </p>
+         * @return a new {@link GiniVisionFeatureConfiguration} instance
+         */
         public GiniVisionFeatureConfiguration build() {
             return new GiniVisionFeatureConfiguration(this);
         }
@@ -79,6 +129,17 @@ public class GiniVisionFeatureConfiguration implements Parcelable {
             return mDocumentImportEnabledFileTypes;
         }
 
+        /**
+         * <p>
+         *     Enable and configure the document import feature or disable it by passing in
+         *     {@link DocumentImportEnabledFileTypes#NONE}.
+         * </p>
+         * <p>
+         *     Disabled by default.
+         * </p>
+         * @param documentImportEnabledFileTypes file types to be enabled for document import
+         * @return the {@link Builder} instance
+         */
         @NonNull
         public Builder setDocumentImportEnabledFileTypes(
                 @NonNull final DocumentImportEnabledFileTypes documentImportEnabledFileTypes) {
@@ -90,6 +151,16 @@ public class GiniVisionFeatureConfiguration implements Parcelable {
             return mFileImportEnabled;
         }
 
+        /**
+         * <p>
+         *     Enable/disable the file import feature.
+         * </p>
+         * <p>
+         *     Disabled by default.
+         * </p>
+         * @param fileImportEnabled {@code true} to enable file import
+         * @return the {@link Builder} instance
+         */
         @NonNull
         public Builder setFileImportEnabled(final boolean fileImportEnabled) {
             mFileImportEnabled = fileImportEnabled;
