0e5bdc10891130c7218089f163a015f0d673d611
Alpár Szotyori <a.szotyori@gini.net>
2017-12-05 15:46:39 +0100

    Requesting camera and storage permissions

--- componentapiexample/src/main/java/net/gini/android/vision/component/MainActivity.java
+++ componentapiexample/src/main/java/net/gini/android/vision/component/MainActivity.java
@@ -2,32 +2,36 @@ package net.gini.android.vision.component;
 
 import static net.gini.android.vision.example.ExampleUtil.isIntentActionViewOrSend;
 
-import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import net.gini.android.vision.GiniVisionDebug;
-import net.gini.android.vision.component.camera.standard.CameraExampleActivity;
 import net.gini.android.vision.component.camera.compat.CameraExampleAppCompatActivity;
+import net.gini.android.vision.component.camera.standard.CameraExampleActivity;
+import net.gini.android.vision.example.RuntimePermissionHandler;
+import net.gini.android.vision.requirements.GiniVisionRequirements;
 import net.gini.android.vision.requirements.RequirementReport;
 import net.gini.android.vision.requirements.RequirementsReport;
 
 import java.util.List;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
-    private Button mButtonStartGiniVisionStandard;
     private Button mButtonStartGiniVisionCompat;
-    private TextView mTextGiniVisionLibVersion;
+    private Button mButtonStartGiniVisionStandard;
+    private RuntimePermissionHandler mRuntimePermissionHandler;
     private TextView mTextAppVersion;
+    private TextView mTextGiniVisionLibVersion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +41,7 @@ public class MainActivity extends Activity {
         addInputHandlers();
         setGiniVisionLibDebugging();
         showVersions();
+        createRuntimePermissionsHandler();
         if (savedInstanceState == null) {
             final Intent intent = getIntent();
             if (isIntentActionViewOrSend(intent)) {
@@ -59,28 +64,56 @@ public class MainActivity extends Activity {
                 .setPositiveButton("Compat", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(final DialogInterface dialogInterface, final int i) {
-                        startGiniVisionCompat(importedFileIntent);
-                        finish();
+                        startGiniVisionCompatForImportedFile(importedFileIntent);
                     }
                 })
                 .setNegativeButton("Standard", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(final DialogInterface dialogInterface, final int i) {
-                        startGiniVisionStandard(importedFileIntent);
-                        finish();
+                        startGiniVisionStandardForImportedFile(importedFileIntent);
                     }
                 }).show();
     }
 
-    private void showVersions() {
-        mTextGiniVisionLibVersion.setText("Gini Vision Library v" + net.gini.android.vision.BuildConfig.VERSION_NAME);
-        mTextAppVersion.setText("v" + BuildConfig.VERSION_NAME);
+    private void startGiniVisionStandardForImportedFile(@NonNull final Intent importedFileIntent) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            Toast.makeText(this, "Component API Standard requires API Level 17 or higher",
+                    Toast.LENGTH_SHORT).show();
+            return;
+        }
+        mRuntimePermissionHandler.requestStoragePermission(
+                new RuntimePermissionHandler.Listener() {
+                    @Override
+                    public void permissionGranted() {
+                        final Intent intent = new Intent(importedFileIntent);
+                        intent.setClass(MainActivity.this, CameraExampleActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+
+                    @Override
+                    public void permissionDenied() {
+                        finish();
+                    }
+                });
     }
 
-    private void setGiniVisionLibDebugging() {
-        if (BuildConfig.DEBUG) {
-            GiniVisionDebug.enable();
-        }
+    private void startGiniVisionCompatForImportedFile(@Nullable final Intent importedFileIntent) {
+        mRuntimePermissionHandler.requestStoragePermission(
+                new RuntimePermissionHandler.Listener() {
+                    @Override
+                    public void permissionGranted() {
+                        final Intent intent = new Intent(importedFileIntent);
+                        intent.setClass(MainActivity.this, CameraExampleAppCompatActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+
+                    @Override
+                    public void permissionDenied() {
+                        finish();
+                    }
+                });
     }
 
     private void addInputHandlers() {
@@ -98,63 +131,57 @@ public class MainActivity extends Activity {
         });
     }
 
-    private void startGiniVisionStandard() {
-        startGiniVisionStandard(null);
+    private void startGiniVisionCompat() {
+        mRuntimePermissionHandler.requestCameraPermission(
+                new RuntimePermissionHandler.Listener() {
+                    @Override
+                    public void permissionGranted() {
+                        // NOTE: on Android 6.0 and later the camera permission is required before checking the requirements
+                        final RequirementsReport report = GiniVisionRequirements.checkRequirements(
+                                MainActivity.this);
+                        if (!report.isFulfilled()) {
+                            showUnfulfilledRequirementsToast(report);
+                            return;
+                        }
+                        final Intent intent = new Intent(MainActivity.this,
+                                CameraExampleAppCompatActivity.class);
+                        startActivity(intent);
+                    }
+
+                    @Override
+                    public void permissionDenied() {
+
+                    }
+                });
     }
 
-    private void startGiniVisionStandard(@Nullable final Intent importedFileIntent) {
+    private void startGiniVisionStandard() {
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR1) {
             Toast.makeText(this, "Component API Standard requires API Level 17 or higher",
                     Toast.LENGTH_SHORT).show();
             return;
         }
+        mRuntimePermissionHandler.requestCameraPermission(
+                new RuntimePermissionHandler.Listener() {
+                    @Override
+                    public void permissionGranted() {
+                        // NOTE: on Android 6.0 and later the camera permission is required before checking the requirements
+                        final RequirementsReport report = GiniVisionRequirements.checkRequirements(
+                                MainActivity.this);
+                        if (!report.isFulfilled()) {
+                            showUnfulfilledRequirementsToast(report);
+                            return;
+                        }
+                        final Intent intent = new Intent(MainActivity.this,
+                                CameraExampleActivity.class);
+                        startActivity(intent);
+                    }
 
-        // Uncomment to enable requirements check.
-        // NOTE: on Android 6.0 and later the camera permission is required before checking the requirements
-//        RequirementsReport report = GiniVisionRequirements.checkRequirements(this);
-//        if (!report.isFulfilled()) {
-//            showUnfulfilledRequirementsToast(report);
-//            return;
-//        }
+                    @Override
+                    public void permissionDenied() {
 
-        final Intent intent;
-        if (importedFileIntent == null) {
-            intent = new Intent(this, CameraExampleActivity.class);
-        } else {
-            intent = new Intent(importedFileIntent);
-            intent.setClass(this, CameraExampleActivity.class);
-        }
-        startActivity(intent);
-    }
-
-    private void startGiniVisionCompat() {
-        startGiniVisionCompat(null);
-    }
-
-    private void startGiniVisionCompat(@Nullable final Intent importedFileIntent) {
-        // Uncomment to enable requirements check.
-        // NOTE: on Android 6.0 and later the camera permission is required before checking the requirements
-//        RequirementsReport report = GiniVisionRequirements.checkRequirements(this);
-//        if (!report.isFulfilled()) {
-//            showUnfulfilledRequirementsToast(report);
-//            return;
-//        }
-
-        final Intent intent;
-        if (importedFileIntent == null) {
-            intent = new Intent(this, CameraExampleAppCompatActivity.class);
-        } else {
-            intent = new Intent(importedFileIntent);
-            intent.setClass(this, CameraExampleAppCompatActivity.class);
-        }
-        startActivity(intent);
-    }
-
-    private void bindViews() {
-        mButtonStartGiniVisionStandard = (Button) findViewById(R.id.button_start_gini_vision_standard);
-        mButtonStartGiniVisionCompat = (Button) findViewById(R.id.button_start_gini_vision_compat);
-        mTextGiniVisionLibVersion = (TextView) findViewById(R.id.text_gini_vision_version);
-        mTextAppVersion = (TextView) findViewById(R.id.text_app_version);
+                    }
+                });
     }
 
     private void showUnfulfilledRequirementsToast(RequirementsReport report) {
@@ -173,6 +200,41 @@ public class MainActivity extends Activity {
                 }
             }
         }
-        Toast.makeText(this, "Requirements not fulfilled:\n" + stringBuilder, Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "Requirements not fulfilled:\n" + stringBuilder,
+                Toast.LENGTH_LONG).show();
+    }
+
+    private void bindViews() {
+        mButtonStartGiniVisionStandard = (Button) findViewById(
+                R.id.button_start_gini_vision_standard);
+        mButtonStartGiniVisionCompat = (Button) findViewById(R.id.button_start_gini_vision_compat);
+        mTextGiniVisionLibVersion = (TextView) findViewById(R.id.text_gini_vision_version);
+        mTextAppVersion = (TextView) findViewById(R.id.text_app_version);
+    }
+
+    private void createRuntimePermissionsHandler() {
+        mRuntimePermissionHandler = RuntimePermissionHandler
+                .forActivity(this)
+                .withCameraPermissionDeniedMessage(
+                        getString(R.string.camera_permission_denied_message))
+                .withCameraPermissionRationale(getString(R.string.camera_permission_rationale))
+                .withStoragePermissionDeniedMessage(
+                        getString(R.string.storage_permission_denied_message))
+                .withStoragePermissionRationale(getString(R.string.storage_permission_rationale))
+                .withGrantAccessButtonTitle(getString(R.string.grant_access))
+                .withCancelButtonTitle(getString(R.string.cancel))
+                .build();
+    }
+
+    private void setGiniVisionLibDebugging() {
+        if (BuildConfig.DEBUG) {
+            GiniVisionDebug.enable();
+        }
+    }
+
+    private void showVersions() {
+        mTextGiniVisionLibVersion.setText(
+                "Gini Vision Library v" + net.gini.android.vision.BuildConfig.VERSION_NAME);
+        mTextAppVersion.setText("v" + BuildConfig.VERSION_NAME);
     }
 }
