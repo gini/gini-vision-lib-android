3393c0a56a32dfbc1a4571b70a0cecbf8370887c
Alpár Szotyori <a.szotyori@gini.net>
2020-01-24 12:07:28 +0100

    Using dot decimal separator when creating line item's raw amount, and
    accepting dot and comma when parsing the raw amount

--- ginivision/src/test/java/net/gini/android/vision/returnassistant/LineItemTest.kt
+++ ginivision/src/test/java/net/gini/android/vision/returnassistant/LineItemTest.kt
@@ -5,6 +5,8 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.google.common.truth.Truth.assertThat
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.math.BigDecimal
+import java.util.*
 
 /**
  * Created by Alpar Szotyori on 17.12.2019.
@@ -27,5 +29,49 @@ class LineItemTest {
 
         // Then
         assertThat(fromParcel).isEqualTo(orig)
+    }
+
+    @Test
+    fun `should parse raw amount with dot decimal separator`() {
+        // Given
+        val lineItem = LineItem("id", "description", 3, "12.13:EUR")
+
+        // Then
+        assertThat(lineItem.amount).isEqualTo(BigDecimal("12.13"))
+        assertThat(lineItem.totalAmount).isEqualTo(BigDecimal("36.39"))
+        assertThat(lineItem.currency).isEqualTo(Currency.getInstance("EUR"))
+        assertThat(lineItem.rawCurrency).isEqualTo("EUR")
+    }
+
+    @Test
+    fun `should parse raw amount with comma decimal separator`() {
+        // Given
+        val lineItem = LineItem("id", "description", 3, "12,13:EUR")
+
+        // Then
+        assertThat(lineItem.amount).isEqualTo(BigDecimal("12.13"))
+        assertThat(lineItem.totalAmount).isEqualTo(BigDecimal("36.39"))
+        assertThat(lineItem.currency).isEqualTo(Currency.getInstance("EUR"))
+        assertThat(lineItem.rawCurrency).isEqualTo("EUR")
+    }
+
+    @Test
+    fun `should create raw amount in english format`() {
+        // Given
+        val rawAmount = LineItem.createRawAmount(BigDecimal("12.13"), "EUR")
+
+        // Then
+        assertThat(rawAmount).isEqualTo("12.13:EUR")
+    }
+
+    @Test
+    fun `should default amount to 0 when the raw amount format is not supported`() {
+        val lineItem = LineItem("id", "description", 3, "100,200.13:EUR")
+
+        // Then
+        assertThat(lineItem.amount).isEqualTo(BigDecimal("0"))
+        assertThat(lineItem.totalAmount).isEqualTo(BigDecimal("0"))
+        assertThat(lineItem.currency).isEqualTo(Currency.getInstance("EUR"))
+        assertThat(lineItem.rawCurrency).isEqualTo("EUR")
     }
 }
