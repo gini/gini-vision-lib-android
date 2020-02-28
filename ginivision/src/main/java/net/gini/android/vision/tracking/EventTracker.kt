1a90a9c0aec1a966b5d154d01b8cb242a7f9b8dc
Alpár Szotyori <a.szotyori@gini.net>
2020-02-28 15:54:59 +0100

    Added the predefined events and the event tracker interface

---
+++ ginivision/src/main/java/net/gini/android/vision/tracking/EventTracker.kt
@@ -0,0 +1,47 @@
+package net.gini.android.vision.tracking
+
+import net.gini.android.vision.GiniVision
+
+/**
+ * Created by Alpar Szotyori on 27.02.2020.
+ *
+ * Copyright (c) 2020 Gini GmbH.
+ */
+
+interface EventTracker {
+    fun onOnboardingScreenEvent(event: Event<OnboardingScreenEvent>)
+    fun onCameraScreenEvent(event: Event<CameraScreenEvent>)
+    fun onReviewScreenEvent(event: Event<ReviewScreenEvent>)
+    fun onAnalysisScreenEvent(event: Event<AnalysisScreenEvent>)
+}
+
+class Event<T : Enum<T>>(val type: T,
+                         val details: Map<String, String> = emptyMap())
+
+@JvmOverloads
+fun trackOnboardingScreenEvent(event: OnboardingScreenEvent, details: Map<String, String> = emptyMap()) {
+    if (GiniVision.hasInstance()) {
+        GiniVision.getInstance().eventTracker.onOnboardingScreenEvent(Event(event, details))
+    }
+}
+
+@JvmOverloads
+fun trackCameraScreenEvent(event: CameraScreenEvent, details: Map<String, String> = emptyMap()) {
+    if (GiniVision.hasInstance()) {
+        GiniVision.getInstance().eventTracker.onCameraScreenEvent(Event(event, details))
+    }
+}
+
+@JvmOverloads
+fun trackReviewScreenEvent(event: ReviewScreenEvent, details: Map<String, String> = emptyMap()) {
+    if (GiniVision.hasInstance()) {
+        GiniVision.getInstance().eventTracker.onReviewScreenEvent(Event(event, details))
+    }
+}
+
+@JvmOverloads
+fun trackAnalysisScreenEvent(event: AnalysisScreenEvent, details: Map<String, String> = emptyMap()) {
+    if (GiniVision.hasInstance()) {
+        GiniVision.getInstance().eventTracker.onAnalysisScreenEvent(Event(event, details))
+    }
+}
