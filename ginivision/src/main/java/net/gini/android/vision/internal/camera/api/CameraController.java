c96b86d5299cf7ba810def92f6d83626a663767d
Alexandru Gabor <alexandru.gabor@halcyonmobile.com>
2021-01-13 14:00:36 +0200

    Log cancel auto focus failure
    (cherry picked from commit e4fba084d83bf845eec145a49a0bc29210cd9320)

--- ginivision/src/main/java/net/gini/android/vision/internal/camera/api/CameraController.java
+++ ginivision/src/main/java/net/gini/android/vision/internal/camera/api/CameraController.java
@@ -276,7 +276,7 @@ public class CameraController implements CameraInterface {
             try {
                 mCamera.cancelAutoFocus();
             } catch (RuntimeException exception) {
-                // do nothing
+                LOG.error("Failed to cancel autofocus");
             }
         }
     }
