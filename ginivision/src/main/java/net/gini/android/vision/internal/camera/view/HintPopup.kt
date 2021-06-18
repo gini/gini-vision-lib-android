42b211191e64903e495c0fcd72184f2dec5fb5b5
Alpar Szotyori <alpar@gini.net>
2021-06-18 18:38:02 +0200

    Extract hint popup related logic into a dedicated class

---
+++ ginivision/src/main/java/net/gini/android/vision/internal/camera/view/HintPopup.kt
@@ -0,0 +1,97 @@
+package net.gini.android.vision.internal.camera.view
+
+import android.view.View
+import androidx.core.view.ViewCompat
+import androidx.core.view.ViewPropertyAnimatorCompat
+import androidx.core.view.ViewPropertyAnimatorListenerAdapter
+
+/**
+ * Internal use only.
+ *
+ * @suppress
+ */
+internal class HintPopup(
+        private val popupView: View,
+        private val popupArrow: View,
+        closeButton: View,
+        private val animationDuration: Long,
+        private val onCloseClicked: () -> Unit) {
+
+    private var popupAnimation: ViewPropertyAnimatorCompat? = null
+    private var popupArrowAnimation: ViewPropertyAnimatorCompat? = null
+
+    var isShown = false
+        private set
+
+    init {
+        closeButton.setOnClickListener {
+            onCloseClicked()
+        }
+    }
+
+    fun show() {
+        if (popupView.alpha != 0f) {
+            return;
+        }
+
+        popupView.visibility = View.VISIBLE
+        popupArrow.visibility = View.VISIBLE
+        clearUploadHintPopUpAnimations()
+        popupArrowAnimation = ViewCompat.animate(
+                popupView)
+                .alpha(1f)
+                .setDuration(animationDuration)
+                .apply {
+                    isShown = true
+                    start()
+                }
+        popupAnimation = ViewCompat.animate(
+                popupArrow)
+                .alpha(1f)
+                .setDuration(animationDuration)
+                .apply {
+                    start()
+                }
+    }
+
+    fun hide(animatorListener: ViewPropertyAnimatorListenerAdapter?) {
+        if (popupView.alpha != 1f) {
+            animatorListener?.onAnimationEnd(popupView)
+        }
+
+        clearUploadHintPopUpAnimations()
+        popupArrowAnimation = ViewCompat.animate(popupView)
+                .alpha(0f)
+                .setDuration(animationDuration)
+                .setListener(object : ViewPropertyAnimatorListenerAdapter() {
+                    override fun onAnimationEnd(view: View) {
+                        isShown = false
+                        popupArrow.visibility = View.GONE
+                        popupView.visibility = View.GONE
+                        animatorListener?.onAnimationEnd(view)
+                    }
+                })
+                .apply {
+                    start()
+                }
+        popupAnimation = ViewCompat.animate(popupArrow)
+                .alpha(0f)
+                .setDuration(animationDuration)
+                .apply {
+                    start()
+                }
+    }
+
+    private fun clearUploadHintPopUpAnimations() {
+        popupArrowAnimation?.apply {
+            cancel()
+            popupView.clearAnimation()
+            setListener(null)
+        }
+        popupAnimation?.apply {
+            cancel()
+            popupView.clearAnimation()
+            setListener(null)
+        }
+    }
+}
